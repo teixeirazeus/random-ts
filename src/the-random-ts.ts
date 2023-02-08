e93dab1eeae460f01436a89b523281cf3f115243
teixeirazeus <teixeira.zeus@gmail.com>
2023-02-08 11:25:30 -0300

    refact one declaration per line

--- src/the-random-ts.ts
+++ src/the-random-ts.ts
@@ -63,8 +63,8 @@ function randomColor(): string {
 }
 
 function randomGaussian(mean: number = 0, standardDeviation: number = 1): number {
-  let u = 0,
-    v = 0
+  let u = 0
+  let v = 0
   while (u === 0) u = Math.random()
   while (v === 0) v = Math.random()
   let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v)
